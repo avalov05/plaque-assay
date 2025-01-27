4ba5a70b3dca3c6915d4c36e50b6ad74a61b5224
avalov05 <anton.valov05@gmail.com>
2025-01-27 01:37:50 -0500

    feat: next setup not working

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# plaque-assay
--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,15 +1,11 @@
 'use client';
-
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import Model from './Model';
 
 
-export default function Scene() 
-{
-    return (
-        <Canvas>
-            <Model />
-        </Canvas>
-    )
+export default function Scene() {
+  return (
+    <Canvas>
+    </Canvas>
+  );
 }
